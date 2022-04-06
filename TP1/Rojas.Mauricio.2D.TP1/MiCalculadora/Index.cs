f36d8b3dba445bc54c1ed710f09f6c5052680008
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-04-06 11:34:57 -0300

    TP1

---
+++ TP1/Rojas.Mauricio.2D.TP1/MiCalculadora/Index.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiCalculadora
+{
+    public partial class Index : Form
+    {
+        public Index()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
