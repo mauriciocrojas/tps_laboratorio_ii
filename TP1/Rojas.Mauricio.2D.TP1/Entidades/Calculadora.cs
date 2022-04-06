059a9cbf615fafa83856ff0aebd8b593af1960f0
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-04-06 12:49:14 -0300

    TP01

--- TP1/Rojas.Mauricio.2D.TP1/Entidades/Calculadora.cs
+++ TP1/Rojas.Mauricio.2D.TP1/Entidades/Calculadora.cs
@@ -4,5 +4,29 @@ namespace Entidades
 {
     public class Calculadora
     {
+
+
+        //Metodos
+
+        public static double Operar (Operando num1, Operando num2, char operador)
+        {
+            char signo = ValidarOperador(operador);
+
+            if (signo == '+')
+            {
+                return num1 + num2;
+            }
+            
+        }
+
+        private static char ValidarOperador (char operador)
+        {
+            if(operador == '+' || operador == '-' || operador == '/' || operador == '*')
+            {
+                return operador;
+            }
+
+            return '+';
+        }
     }
 }
--- TP1/Rojas.Mauricio.2D.TP1/Entidades/Operando.cs
+++ TP1/Rojas.Mauricio.2D.TP1/Entidades/Operando.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entidades
+﻿namespace Entidades
 {
-    internal class Operando
+    public class Operando
     {
+        private double numero;
+
+        private Operando()
+        {
+            numero = 0;
+        }
+
+        //Metodos
+
+        private double ValidarOperando(string strNumero)
+        {
+            if (int.TryParse(strNumero, out int auxDouble))
+            {
+                return auxDouble;
+            }
+            return 0;
+        }
+
+        // Setea valor del numero
+        private string Numero
+        {
+            set
+            {
+                this.numero = ValidarOperando(value);
+            }
+        }
+
+        // Comprueba que el string pasado sea un numero binario, retorna false si no lo es, true si lo es
+        private bool EsBinario(string binario)
+        {
+            for (int i = 0; i < binario.Length - 1; i++)
+            {
+                if (binario[i] == '0' || binario[i] == '1')
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
     }
 }
