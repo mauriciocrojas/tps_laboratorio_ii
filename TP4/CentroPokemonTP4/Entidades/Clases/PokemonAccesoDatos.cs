f43ba4f2d84bafe44da3ee54ebce89b3a83061b0
mauriciocrojas <mauriciocc.rojas@gmail.com>
2022-06-18 12:31:44 -0300

    instalacion paquete nugget System.Data.SqlClient

---
+++ TP4/CentroPokemonTP4/Entidades/Clases/PokemonAccesoDatos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Clases
+{
+    public static class PokemonAccesoDatos
+    {
+        static string connectionString;
+        static SqlCommand command;
+
+        static PokemonAccesoDatos()
+        {
+            connectionString = @"Data Source=.;Initial Catalog=CentroPokemon;Integrated Security=True";
+        }
+
+    }
+}
